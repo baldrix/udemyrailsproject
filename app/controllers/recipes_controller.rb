2e7c0950f8591d038ad7229ecf66333bae5174d3
baldrix <xbaldric@outlook.com>
2015-10-02 23:58:54 +0000

    Added error handler for recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -16,7 +16,6 @@ class RecipesController < ApplicationController
   def create
     @recipe = Recipe.new(recipe_params)
     @recipe.chef = Chef.find(2)
-    #@recipe.save
     if @recipe.save
       flash[:success] = "Your recipe was created successfully"
       redirect_to recipes_path
